411f3f1ffaad52d8d33b027740a200925faf1dda
Nikola Sirakov <156999301+nsirakow@users.noreply.github.com>
2024-02-08 10:51:09 +0200

    Create CardRanks.java

---
+++ Abstraction/CardRanks/CardRanks.java
@@ -0,0 +1,24 @@
+public enum CardRanks {
+  
+    ACE(10),
+    TWO(20),
+    THREE(30),
+    FOUR(40),
+    FIVE(50),
+    SIX(60),
+    SEVEN(70),
+    EIGHT(80),
+    NINE(90),
+    TEN(100),
+    JACK(110),
+    QUEEN(120),
+    KING(130);
+    final int CardRanksNumbers;
+  
+    CardRanks(int cardRanksNumbers){
+        this.CardRanksNumbers = cardRanksNumbers;
+    }
+    public int getCardRanksNumbers() {
+        return CardRanksNumbers;
+    }
+}
