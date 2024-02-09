e539c8bf7d5108dbcff170cc19841d84bc30b9cd
Nikola Sirakov <156999301+nsirakow@users.noreply.github.com>
2024-02-09 16:51:38 +0200

    Create RankPowers.java

---
+++ Abstraction/CardsWithPower/RankPowers.java
@@ -0,0 +1,24 @@
+public enum RankPowers {
+  
+    ACE(14),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(11),
+    QUEEN(12),
+    KING(13);
+    final int rankPowerNumbers;
+  
+    RankPowers(int rankPowerNumbers){
+        this.rankPowerNumbers = rankPowerNumbers;
+    }
+    public int getRankPowerNumbers() {
+        return rankPowerNumbers;
+    }
+}
