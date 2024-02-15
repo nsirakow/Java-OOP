ed1bd0f8e8b94172ee1e68a9f6f32d577c4b8b0a
Nikola Sirakov <156999301+nsirakow@users.noreply.github.com>
2024-02-15 10:05:36 +0200

    Create Kitten.java

---
+++ Inheritance/AnimalsHierarchy/Animal/Animals/Cats/Kitten.java
@@ -0,0 +1,18 @@
+package Animal.Animals.Cats;
+import Animal.Animals.Cat;
+
+public class Kitten extends Cat {
+  
+    public Kitten(String name, int age) {
+        super(name, age, "Female");
+    }
+  
+    public Kitten (String name, int age, String gender){
+        super(name, age, "Female");
+    }
+  
+    @Override
+    public String produceSound() {
+        return ("Meow");
+    }
+}
